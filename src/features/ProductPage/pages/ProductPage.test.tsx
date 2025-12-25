05bf4a5b4e1e29a9fc0ae2ed593d18469064eaa3
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-12-25 16:02:50 +0800

    test(product-page): add test case when it renders manga data when given a mangaId

---
+++ src/features/ProductPage/pages/ProductPage.test.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, it, vi } from 'vitest';
+import { screen, render, waitFor } from '@testing-library/react';
+import { mangaService } from '../service/mangaService';
+import type { JikanManga } from '@/features/HomePage/types/JikanManga';
+import { MemoryRouter, Route, Routes } from 'react-router';
+import { mapMangaToProduct } from '@/features/HomePage/utils/mapMangaToProduct';
+import { ProductPage } from './ProductPage';
+
+const mockMangaData: JikanManga = {
+  mal_id: 1,
+  images: {
+    jpg: {
+      image_url: 'image.jpg',
+    },
+  },
+  title: 'One Piece',
+  score: 9.22,
+  genres: [{ name: 'Action' }],
+  synopsis:
+    'One Piece follows Monkey D. Luffy, a young, rubber-powered pirate inspired by his childhood hero to find the legendary "One Piece" treasure left behind by Gol D. Roger.',
+};
+
+describe('ProductPage', () => {
+  it('renders manga data when given a mangaId', async () => {
+    vi.spyOn(mangaService, 'fetchMangaData').mockResolvedValueOnce({
+      data: mockMangaData,
+    });
+
+    const mappedMangaData = mapMangaToProduct(mockMangaData);
+
+    render(
+      <MemoryRouter initialEntries={['/manga/1']}>
+        <Routes>
+          <Route path='/manga/:mangaId' element={<ProductPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // Make sure we're not stuck in error state
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+
+    await screen.findByRole('heading', { name: mappedMangaData.title });
+
+    expect(screen.getByText(mappedMangaData.description)).toBeInTheDocument();
+  });
+});
